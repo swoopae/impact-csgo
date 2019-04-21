21cc045e1ab6f998840f9a4e3b251bad413cd1ad
octav adrian <senpaifuuzy@gmail.com>
2019-04-21 22:13:12 +0300

    fix getting active weapon of entity

--- purpura sdk/sdk/entities.cpp
+++ purpura sdk/sdk/entities.cpp
@@ -2,8 +2,8 @@
 #include "i_base_handle.h"
 #include "../interfaces/interfaces.h"
 
-// this is incorrect please fix later owo cutie
 c_base_combat_weapon* c_base_entity::active_weapon() {
-	const auto weapon_data = get_value < unsigned long >(netvar_manager->get_offset("DT_BaseCombat_Character", "m_hActiveWeapon"));
+	static int offset = netvar_manager->get_offset("DT_BaseCombatCharacter", "m_hActiveWeapon");
+	const auto weapon_data = get_value<c_base_handle>(offset);
 	return reinterpret_cast<c_base_combat_weapon*>(interfaces::client_ent_list->get_client_entity_from_handle(weapon_data));
 }
